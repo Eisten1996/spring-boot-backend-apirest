e685b032698a9cb0642691e0c86cb7937bca305f
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-30 21:20:16 -0500

    Creando Proyecto Backend API REST

---
+++ src/main/java/com/dipper/springboot/backend/apirest/app/SpringBootBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.dipper.springboot.backend.apirest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendApirestApplication.class, args);
+    }
+
+}
