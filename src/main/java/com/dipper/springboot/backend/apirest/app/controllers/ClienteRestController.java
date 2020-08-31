0f5b56ee507ed6db3adb58b6afffccec35a53be3
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-30 23:52:34 -0500

    Uso de Cors para compartir recursos en API REST

--- src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
@@ -3,12 +3,14 @@ package com.dipper.springboot.backend.apirest.app.controllers;
 import com.dipper.springboot.backend.apirest.app.models.entity.Cliente;
 import com.dipper.springboot.backend.apirest.app.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
