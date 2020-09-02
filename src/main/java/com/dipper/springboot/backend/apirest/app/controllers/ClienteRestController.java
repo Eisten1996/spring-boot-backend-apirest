44c71199dc583b7392e204ca5cc72f4e41c123c0
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-02 17:49:05 -0500

    Escribiendo los métodos show y create en el Controlador Backend API Rest

--- src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
@@ -3,11 +3,10 @@ package com.dipper.springboot.backend.apirest.app.controllers;
 import com.dipper.springboot.backend.apirest.app.models.entity.Cliente;
 import com.dipper.springboot.backend.apirest.app.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
@@ -19,7 +18,18 @@ public class ClienteRestController {
     private IClienteService clienteService;
 
     @GetMapping("/clientes")
-    public List<Cliente> index() {
+    public List<Cliente> showAllCliente() {
         return clienteService.findAll();
+    }
+
+    @GetMapping("/clientes/{id}")
+    public Cliente showCliente(@PathVariable Long id) {
+        return clienteService.findById(id);
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente createCliente(@RequestBody Cliente cliente) {
+        return clienteService.save(cliente);
     }
 }
--- src/main/java/com/dipper/springboot/backend/apirest/app/models/entity/Cliente.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/models/entity/Cliente.java
@@ -18,6 +18,11 @@ public class Cliente implements Serializable {
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
+    @PrePersist
+    public void prePersist() {
+        createAt = new Date();
+    }
+
 
     public Long getId() {
         return id;
