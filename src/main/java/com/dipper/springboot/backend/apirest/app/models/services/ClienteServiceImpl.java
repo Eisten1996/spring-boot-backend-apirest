6710abaf0b26b01ea6d93a3f01a3fbb8692c5cc5
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-02 17:38:58 -0500

    Escribiendo los métodos del CRUD en la clase ClienteService del Backend

--- src/main/java/com/dipper/springboot/backend/apirest/app/models/services/ClienteServiceImpl.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/models/services/ClienteServiceImpl.java
@@ -19,4 +19,22 @@ public class ClienteServiceImpl implements IClienteService {
     public List<Cliente> findAll() {
         return (List<Cliente>) clienteDao.findAll();
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Long id) {
+        return clienteDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        clienteDao.deleteById(id);
+    }
 }
--- src/main/java/com/dipper/springboot/backend/apirest/app/models/services/IClienteService.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/models/services/IClienteService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface IClienteService {
 
     public List<Cliente> findAll();
+
+    public Cliente findById(Long id);
+
+    public Cliente save(Cliente cliente);
+
+    public void delete(Long id);
 }
