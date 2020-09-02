80056c5f3dade80ce3ca7888ab60ab69f4dd11df
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-02 17:56:23 -0500

    Escribiendo los métodos update y delete en el Controlador Backend API Rest

--- src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
@@ -31,5 +30,23 @@ public class ClienteRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente createCliente(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
+    }
+
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente updateCliente(@RequestBody Cliente cliente, @PathVariable Long id) {
+        Cliente clienteActual = clienteService.findById(id);
+
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setEmail(cliente.getEmail());
+
+        return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletCliente(@PathVariable Long id) {
+        clienteService.delete(id);
     }
 }
