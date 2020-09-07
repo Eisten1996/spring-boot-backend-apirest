a889bc596326da754d736efdd062434a1924da72
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-07 09:45:22 -0500

    Manejo de error en el Backend en método handler show (obtener por id)

--- src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/controllers/ClienteRestController.java
@@ -3,10 +3,14 @@ package com.dipper.springboot.backend.apirest.app.controllers;
 import com.dipper.springboot.backend.apirest.app.models.entity.Cliente;
 import com.dipper.springboot.backend.apirest.app.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -22,8 +26,22 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Cliente showCliente(@PathVariable Long id) {
-        return clienteService.findById(id);
+    public ResponseEntity<?> showCliente(@PathVariable Long id) {
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", " : ".concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID ".concat(id.toString().concat(" no existe en la base de datos!")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+        }
     }
 
     @PostMapping("/clientes")
