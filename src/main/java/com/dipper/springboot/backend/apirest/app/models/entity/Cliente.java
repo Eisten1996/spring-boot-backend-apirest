f9f87d34f516b8e54b4d4d7fcd8098d957c7645d
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-10-22 17:11:17 -0500

    Anotaciones JavaBeans Validation en la clase Entity

--- src/main/java/com/dipper/springboot/backend/apirest/app/models/entity/Cliente.java
+++ src/main/java/com/dipper/springboot/backend/apirest/app/models/entity/Cliente.java
@@ -1,6 +1,9 @@
 package com.dipper.springboot.backend.apirest.app.models.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,9 +15,16 @@ public class Cliente implements Serializable {
     private Long id;
 
     @Column(nullable = false)
+    @NotEmpty
+    @Size(min = 4, max = 12)
     private String nombre;
+
+    @NotEmpty
     private String apellido;
+
     @Column(nullable = false, unique = true)
+    @NotEmpty
+    @Email
     private String email;
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
